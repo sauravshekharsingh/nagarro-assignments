3ab69420bb0e20e5d44c989b0177d6fac5267091
Saurav Shekhar Singh <62594900+sauravshekharsingh@users.noreply.github.com>
2022-07-25 13:43:57 +0530

    update copy-to-clipboard

--- copy-to-clipboard/script.js
+++ copy-to-clipboard/script.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
+  let text = "";
+
   $("#btn").click(function () {
     $("#text").select();
     document.execCommand("copy");
+    text = $("#text").val();
+  });
+
+  $(".paste-btn").click(function () {
+    $("#paste").val(text);
   });
 });
